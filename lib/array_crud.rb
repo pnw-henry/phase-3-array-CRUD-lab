86feb186141fd4c207c59a560a992147e7aa0e3e
Henry Escobar <henrye@gmail.com>
2022-10-11 17:40:23 -0700

    commit tree 783a5ee1fc0b80ade014a7169646428015ff32f3

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
 def create_an_empty_array
-  
+  first_array = []
+  first_array
 end
 
 def create_an_array
-  
+  second_array = [1, 2, 3, 4]
+  second_array
+
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
+  array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
+  array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
+    array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
